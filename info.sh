a1df5e949a33b37c1c6be09499c2f85e10e3aa9e
devopsteach <devopsteach@gmail.com>
2021-06-16 10:57:21 +0530

    added os info into info script

--- info.sh
+++ info.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 echo "this scrit to collect information about system and its resources"
+
+OS_INFO=$(cat /etc/os-release | head -1 | awk -F'"' '{print $2}')
+
+echo "my Operating System = ${OS_INFO}"
