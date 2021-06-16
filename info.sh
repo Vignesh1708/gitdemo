454646f0be081c9300afed3920f4f4204dec7920
devopsteach <devopsteach@gmail.com>
2021-06-16 11:19:47 +0530

    reset

--- info.sh
+++ info.sh
@@ -5,7 +5,3 @@ echo "this scrit to collect information about system and its resources"
 OS_INFO=$(cat /etc/os-release | head -1 | awk -F'"' '{print $2}')
 
 echo "my Operating System = ${OS_INFO}"
-
-CPU_INFO=$(cat /proc/cpuinfo | grep -i processor | wc -ldfdf)
-
-echo "my Available CPU Core = ${CPU_INFO}"
