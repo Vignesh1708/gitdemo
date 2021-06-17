ccffb0dcfef82abdf432c00c17cb155f1f5c954b
devopsteach <devopsteach@gmail.com>
2021-06-17 10:46:13 +0530

    adding memory info

--- info.sh
+++ info.sh
@@ -9,3 +9,9 @@ echo "my Operating System = ${OS_INFO}"
 CPU_INFO=$(cat /proc/cpuinfo | grep -i processor | wc -l)
 
 echo "My CPU Cores = ${CPU_INFO}"
+
+MEM_INFO=$(free -h | awk '{print $2}' | head -2 | tail -1)
+
+echo "My Total Memory = ${MEM_INFO}"
+
+echo "This is all out resource available in my server"
